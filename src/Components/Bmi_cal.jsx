713a6f936323894790f6c794ff495a02ccca00a9
stevendev97 <x.limsteven@gmail.com>
2024-10-03 15:40:34 -0500

    added loader and condition render

--- src/Components/Bmi_cal.jsx
+++ src/Components/Bmi_cal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Bmi_cal.css'
+import { BounceLoader } from 'react-spinners';
 import underwegith from '../assets/bmi_chart/underweigth.png'
 import overweight from '../assets/bmi_chart/overweight.png'
 import obese from '../assets/bmi_chart/obese.png'
@@ -10,6 +11,7 @@ function Bmi_cal() {
     const [inch, setInch] = useState(0)
     const [lbs, setLbs] = useState(0)
     const [bmiScore, setBmiScore] = useState(0)
+    const [isLoading, setisLoding] = useState(false)
 
     const url = `https://nutrition-calculator.p.rapidapi.com/api/bmi?measurement_units=std&feet=${feet}&inches=${inch}&lbs=${lbs}`;
     const options = {
@@ -27,6 +29,7 @@ function Bmi_cal() {
             const result = await response.json();
             console.log(result);
             setBmiScore(result)
+            setisLoding(false)
         } catch (error) {
             console.error(error);
         }
@@ -37,28 +40,50 @@ function Bmi_cal() {
             <h2>BMI Calculator</h2>
             <p>BMI, or Body Mass Index, is a measure that uses height and weight to estimate body fat.</p>
 
-            <form id='bmi_form' onSubmit={() => {
-                event.preventDefault()
-                console.log(feet, inch, lbs)
-                get_bmi()
-            }}>
+            {isLoading ? <BounceLoader id='loader' /> : bmiScore !== 0 ?
 
-                <h4>Height</h4>
-                <label>Feet</label>
-                <input type='number' value={feet} onChange={(e) => setFeet(e.target.value)}></input>
-                <label>Inches</label>
-                <input type='number' value={inch} onChange={(e) => setInch(e.target.value)}></input>
+                <>
+                    <h3>Your BMI Score is {bmiScore.bmi} </h3>
 
-                <h4>Weight</h4>
-                <label>lbs</label>
-                <input type='number' value={lbs} onChange={(e) => setLbs(e.target.value)}></input>
+                    <div id='chart_container'>
+
+                        <div className={`chart_box ${bmiScore.bmi <= 18.5 ? 'selected' : ''}`}>
+                            <img src={underwegith} />
+                        </div>
+                        <div className={`chart_box ${bmiScore.bmi > 18.5 && bmiScore.bmi <= 25 ? 'selected' : ''}`}>
+                            <img src={normal} />
+                        </div>
+                        <div className={`chart_box ${bmiScore.bmi > 25 && bmiScore.bmi <= 30 ? 'selected' : ''}`}>
+                            <img src={overweight} />
+                        </div>
+                        <div className={`chart_box ${bmiScore.bmi > 30 ? 'selected' : ''}`}>
+                            <img src={obese} />
+                        </div>
+                    </div>
+
+                    <button id='form_btn' onClick={() => setBmiScore(0)}>New Form</button>
+                </> :
+                <form id='bmi_form' onSubmit={() => {
+                    event.preventDefault()
+                    setisLoding(true)
+                    console.log(feet, inch, lbs)
+                    get_bmi()
+                }}>
+
+                    <h4>Height</h4>
+                    <label>Feet</label>
+                    <input type='number' value={feet} onChange={(e) => setFeet(e.target.value)}></input>
+                    <label>Inches</label>
+                    <input type='number' value={inch} onChange={(e) => setInch(e.target.value)}></input>
+
+                    <h4>Weight</h4>
+                    <label>lbs</label>
+                    <input type='number' value={lbs} onChange={(e) => setLbs(e.target.value)}></input>
 
 
-                <button id='form_btn'>Check</button>
-            </form>
+                    <button id='form_btn'>Check</button>
+                </form>}
 
-
-            {/* {bmiScore !== 0 && <h3>{bmiScore.bmi} you're normal weight! </h3>} */}
 
             {/* <div id='chart_container'>
                 <div className='chart_box'>
